0f2705ed52768e918100bdd2c7608cd78fcf5efc
Radu Achim <radu.achim@iteam.se>
2015-05-20 18:36:38 +0200

    Bit of refactoring, save the original tweet to elastic when getting a response from the api

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 var express = require('express');
 var streamer = require('./lib/streamer');
+var parser = require('./lib/parser');
+var poster = require('./lib/poster');
+var elastic = require('./lib/elastic');
+
 var app = express();
 
-app.listen(process.env.PORT ||  3000, streamer.listen);
+
+function onTweet(tweet) {
+  parser
+    .parseTweet(tweet)
+    .then(poster.send)
+    .then(elastic.save.bind(null, tweet))
+    .then(function (result) {
+      console.log('result', result);
+    })
+    .catch(function (error) {
+      console.log('something borked!', error);
+    })
+    .done();
+}
+
+app.listen(process.env.PORT ||  3000, streamer.listen.bind(null, onTweet));
