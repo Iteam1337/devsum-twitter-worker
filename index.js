a50763547b3b476036fdd2631537e9529acc3180
irony <cl@iteam.se>
2015-05-20 22:27:12 +0200

    use socket.io

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 var nconf = require('nconf');
 nconf.env('__').file(process.cwd() + '/config.json');
 
-var express = require('express');
 var streamer = require('./lib/streamer');
 var parser = require('./lib/parser');
 var poster = require('./lib/poster');
 var elastic = require('./lib/elastic');
 var filter = require('./lib/filter');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+
+var io = require('socket.io').listen(process.env.PORT ||  3000);
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+});
+
+io.on('error', function(){ console.log('io error', arguments); });
 
 function onTweet(tweet) {
   parser
@@ -25,10 +29,8 @@ function onTweet(tweet) {
     .done();
 }
 
-app.listen(process.env.PORT ||  3000, function () {
-  streamer
-  .listen(onTweet)
-  .then(function () {
-    console.log('started listening to tweets');
-  });
+streamer
+.listen(onTweet)
+.then(function () {
+  console.log('started listening to tweets');
 });
